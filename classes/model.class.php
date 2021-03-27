a91e12ad53c539eb965fc3c546383f80fe7b7915
Caracoancea Timotei <caracoancea.timotei@gmail.com>
2021-03-27 14:09:50 +0200

    Added SQL database connection

---
+++ classes/model.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class Model extends Dbh{
+
+    protected function getDestinations(){
+        $sql="SELECT * FROM destinations";
+        $stmt=$this->connect()->query($sql);
+        $results=$stmt->fetchAll();
+        return $results;
+    }
+
+    protected function addDest($city,$country){
+        $sql="INSERT INTO destinations(city,country) VALUES(?,?)";
+        $stmt=$this->connect()->prepare($sql);
+        $stmt->execute([$city,$country]);
+    }
+    
+}
---
+++ classes/view.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class View extends Model{
+
+    protected function showDestinations(){
+        $results=$this->getDestinations();
+        
+    }
+
+}
